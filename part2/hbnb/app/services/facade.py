b9141eca4b82e5ec3c05ecf30e03f731ebf02f52
kahlidfma5 <147338247+kahlidfma5@users.noreply.github.com>
2025-06-18 20:59:55 +0300

    Update facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -23,3 +23,16 @@ class HBnBFacade:
         # Logic will be implemented in later tasks
         pass
 
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+    
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data):
