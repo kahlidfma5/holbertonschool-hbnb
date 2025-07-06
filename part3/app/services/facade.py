f979d1e0a01e67e7b7f489c406cd32fa4f486bd5
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-07-06 14:07:48 +0000

    add changes for part3 task 0 and task 1

---
+++ part3/app/services/facade.py
@@ -0,0 +1,78 @@
+from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.review import Review
+
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+    
+    def update_user(self, user_id, user_data):
+        if self.get_user(user_id):
+            self.user_repo.update(user_id, user_data)
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+    
+    def update_amenity(self, amenity_id, amenity_data):
+        if self.get_amenity(amenity_id):
+            self.amenity_repo.update(amenity_id, amenity_data)
+    
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        #place.amenities = place_data['amenities']
+        self.place_repo.add(place)
+        return place 
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        self.place_repo.update(place_id, place_data)
+        
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review 
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+        reviews = self.review_repo.get_all()
+        return [review for review in reviews if review.place_id == place_id]
+
+    def update_review(self, review_id, review_data):
+        self.review_repo.update(review_id, review_data)
+
+    def delete_review(self, review_id):
+        self.review_repo.delete(review_id)
---
+++ part3/tests/test_user_api.py
@@ -0,0 +1,62 @@
+import unittest
+from app import create_app
+import json
+
+class TestUserEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_user(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com"
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_user_invalid_data(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "",
+            "last_name": "",
+            "email": "invalid-email"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_user(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "aaajanedoe@example.com"
+        })
+        
+        created_user = json.loads(response.data)
+        user_id = created_user['id']
+        
+        # Get the user
+        response = self.client.get(f'/api/v1/users/{user_id}')
+        
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_invalid_user(self):
+        response = self.client.get(f'/api/v1/users/invalid_user_id')        
+        self.assertEqual(response.status_code, 404)
+    
+    def test_update_user(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "first name",
+            "last_name": "last name",
+            "email": "user@gmail.com"
+        })
+        created_user = json.loads(response.data)
+        user_id = created_user['id']
+        response = self.client.put(f'/api/v1/users/{user_id}', json={
+            "first_name": "first name",
+            "last_name": "last name",
+            "email": "user@hotmail.com"
+            })
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
